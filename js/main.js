374c26ae0bf77578627d6e76973777018ed1978b
Ryuu76000 <r.lopez76000@gmail.com>
2021-09-24 12:43:09 +0200

    ajout de mots et correction commentaires

--- js/main.js
+++ js/main.js
@@ -1,36 +1,40 @@
-// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Declaration des variables>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-let words = [
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Déclaration des variables>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+let words = [                              
     "concatenation",
-    "javascript",
-    "prototype",
-    "recursive"
+    "javascript",           
+    "prototype",                                         //mots contenus dans le jeu
+    "recursive",
+    "algorithme",
+    "navigateur",
+    "paradigme"
 ];
-let selectedWords = words[Math.floor(Math.random() * words.length)]; //selection aléatoire du mot
+let selectedWords = words[Math.floor(Math.random() * words.length)]; //sélection aléatoire du mot
 let splitWord = selectedWords.split("");                             //éclatement du mot selectionné 
 let hiddenWord = [];                                                 //création d'un tableau vide
-let life = 7;                                                        //nombre de coup de base
-let play = true;                                                     //boolean utilisé pour faire boucler la fonction principale(main)
+let life = 7;                                                        //nombre de coups de base
+let play = true;                                                     //boolean utilisé pour faire boucler la fonction principale (main)
 
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Mise en place des fonctions>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 function underscoreWord() {
-    for (let i = 0; i < selectedWords.length; i++) {                 //incrémentation des underscores dans le tableau vide(même nombre que mot éclaté)
+    for (let i = 0; i < selectedWords.length; i++) {                 //incrémentation des underscores dans le tableau vide (même nombre que mot éclaté)
     selectedWords[i] = hiddenWord.push(" _ ") 
     }
 };
 
 
+//fonction principale où boucle la logique
 
 function Main() {
-    let find ;                                                       //variable utilisé pour définir true ou false sur l'imput du joueur 
+    let find ;                                                       //variable utilisée pour définir true ou false sur l'imput du joueur 
     while (play === true){                                           //boucle principale de la fonction avec comme condition (voir ligne 12)
         find = false;
         let playerImput = prompt( "il vous reste " + life + " tentatives \n"  + hiddenWord ) //demande d'imput au joueur + affichage du mot en underscore
         
-        if  (playerImput.length !== 1) {                             //statement qui verifie qu'il y a bien une seul lettre et relance la fonction si non.                    
+        if  (playerImput.length !== 1) {                             //statement qui vérifie qu'il y a bien une seule lettre et relance la fonction sinon.                    
             Main()
         }
-        if (hiddenWord.join("") === splitWord.join("")) {            // termine le jeu quand le joueur à trouvé le mot + affichage du mot
+        if (hiddenWord.join("") === splitWord.join("")) {            // termine le jeu quand le joueur a trouvé le mot + affichage du mot
             play = false;
             return alert (`Bravo vous avez trouvé le mot ${selectedWords} (>'-')>`)
         }
@@ -50,7 +54,8 @@ function Main() {
     }
 }
 
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Apelle des fonctions>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Apel des fonctions et alert d'arrivé sur le jeu>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+alert ("Bienvenue sur le jeu du pendu à vous de trouver les lettres")
 underscoreWord();
 Main();
